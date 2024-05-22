1cc819380577c637b91ba451f94083d9362b4228
James Thomas <55170239+JT-Censys@users.noreply.github.com>
2024-05-22 11:48:51 -0400

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -3,8 +3,7 @@
         nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
     };
 
-    outputs.packages."x86_64-linux".default = let
-        pkgs = nixpkgs.legacyPackages."x86_64-linux";
+    outputs.packages."x86_64-linux".default = let pkgs = nixpkgs.legacyPackages."x86_64-linux";
     in pkgs.buildEnv {
         name = "work-hackingtools";
         paths = with pkgs; [
